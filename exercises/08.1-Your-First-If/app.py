43d0a5c515b250d7c69607886713217035422087
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-05-21 06:59:02 +0000

    Aprobados ejercicios hasta el 10.1

--- exercises/08.1-Your-First-If/app.py
+++ exercises/08.1-Your-First-If/app.py
@@ -1,3 +1,11 @@
 total = int(input('How much money do you have in your pocket\n'))
 
 # YOUR CODE HERE
+if total > 100:
+    response = "Give me your money!"
+elif total <= 50:
+    response = "You are a poor guy, go away!"
+else:
+    response = "Buy me some coffee you cheap!"
+
+print(response)
